5eb678d48ec7c19e1792ccc6c491ada29b855a69
Pascal Aubry <pascal.aubry@univ-rennes1.fr>
2008-06-11 14:42:03 +0000

    PHPCAS-11 resolve the client IP
    git-svn-id: https://source.jasig.org/cas-clients/phpcas/trunk@15842 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/CAS/client.php
+++ source/CAS/client.php
@@ -966,7 +966,8 @@ class CASClient
 			if (!$allowed_clients) {
 				$allowed_clients = array( $this->getServerHostname() ); 
 			}
-			$client = $_SERVER['REMOTE_HOST'];
+			$client_ip = $_SERVER['REMOTE_ADDR'];
+			$client = gethostbyaddr($client_ip);
 			phpCAS::log("Client: ".$client);
 			$allowed = false;
 			foreach ($allowed_clients as $allowed_client) {
