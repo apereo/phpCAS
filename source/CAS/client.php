978a6e9f83f0408e3928f7416c8ba2283b77ac1a
Brett Bieber <brett.bieber@gmail.com>
2008-09-23 01:05:37 +0000

    NOJIRA Add redirect to location without ticket in the url.
    git-svn-id: https://source.jasig.org/cas-clients/phpcas/trunk@16283 f5dbab47-78f9-eb45-b975-e544023573eb

--- source/CAS/client.php
+++ source/CAS/client.php
@@ -513,6 +513,8 @@ class CASClient
             }
             // restore old session vars
             $_SESSION = $old_session;
+            // Redirect to location without ticket.
+            header('Location: '.$this->getURL());
 		}
 		
 		//activate session mechanism if desired
