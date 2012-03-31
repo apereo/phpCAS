0d9b1a45d762c575b02eb87a3f5c706e5528fd2e
jfritschi <jfritschi@freenet.de>
2012-03-31 09:43:54 +0200

    #30 Fix minimal pear support. Readd the original makepackage.php and fix it to work with the new build system and update to all changes in version 1.3.0

---
+++ utils/makepackage.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Packaging File to create a pear package.xml
+ *
+ * @package CAS
+ * @author  Brett Bieber
+ * @author  Joachim Fritschi
+ */
+error_reporting(E_ALL ^ E_DEPRECATED);
+ini_set('display_errors',true);
+
+/**
+ * Require the PEAR_PackageFileManager2 classes, and other
+ * necessary classes for package.xml file creation.
+ */
+require_once 'PEAR/PackageFileManager2.php';
+require_once 'PEAR/PackageFileManager/File.php';
+require_once 'PEAR/Task/Postinstallscript/rw.php';
+require_once 'PEAR/Config.php';
+require_once 'PEAR/Frontend.php';
+
+/**
+ * @var PEAR_PackageFileManager
+ */
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+chdir(dirname(__FILE__));
+$pfm = new PEAR_PackageFileManager2();
+$pfm->setOptions(array(
+    'packagedirectory'  => '${basedir}/tmp/CAS-${phpcas.version}',
+    'outputdirectory'  => '${basedir}/tmp',
+    'baseinstalldir'    => '/',
+    'filelistgenerator' => 'file',
+    'simpleoutput' => true,
+    'roles'=>array('php'=>'php'),
+    'exceptions'=>array()
+));
+$pfm->setPackage('CAS');
+$pfm->setPackageType('php'); // this is a PEAR-style php script package
+$pfm->setSummary('Central Authentication Service client library in php');
+$pfm->setDescription('This package is a PEAR installable library for using a Central Authentication Service.');
+$pfm->setChannel('__uri');
+$pfm->setAPIStability('${phpcas.apiStability}');
+$pfm->setReleaseStability('${phpcas.releaseStability}');
+$pfm->setAPIVersion('${phpcas.version}');
+$pfm->setReleaseVersion('${phpcas.version}');
+$pfm->setNotes('see https://github.com/Jasig/phpCAS/blob/master/docs/ChangeLog');
+
+$pfm->addMaintainer('lead','fritschi','Joachim Fritschi','jfritschi@freenet.de');
+$pfm->addMaintainer('helper','adamfranco','Adam Franco','afranco@middlebury.edu');
+
+$pfm->setLicense('Apache 2.0 License', 'https://github.com/Jasig/phpCAS/blob/master/LICENSE');
+$pfm->clearDeps();
+$pfm->setPhpDep('5.0.0');
+$pfm->setPearinstallerDep('1.4.3');
+
+$pfm->addExtensionDep('required', 'curl');
+$pfm->addExtensionDep('required', 'openssl');
+$pfm->addExtensionDep('required', 'dom');
+$pfm->addExtensionDep('required', 'zlib');
+$pfm->addExtensionDep('required', 'pdo');
+
+
+$pfm->generateContents();
+$pfm->writePackageFile();
+
