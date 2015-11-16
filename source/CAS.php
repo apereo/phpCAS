f4a5d7238d1245c22d65cc3665e2a172354a0032
jfritschi <jfritschi@freenet.de>
2015-11-16 21:44:51 +0100

    Updated version for development after the 1.3.4 release.

--- source/CAS.php
+++ source/CAS.php
@@ -63,7 +63,7 @@ if (!defined('E_USER_DEPRECATED')) {
 /**
  * phpCAS version. accessible for the user by phpCAS::getVersion().
  */
-define('PHPCAS_VERSION', '1.3.4');
+define('PHPCAS_VERSION', '1.3.4+');
 
 /**
  * @addtogroup public
