842a88cf7a50349ff4086fac3627605e4552ba84
Joachim Fritschi <jfritschi@freenet.de>
2021-02-28 20:51:54 +0000

    Force channel upgrade

--- utils/build.sh
+++ utils/build.sh
@@ -3,6 +3,7 @@
 yum -y update && yum -y install ant doxygen php-pear
 
 cd /app/utils \
+&& pear channel-update pear \
 && pear upgrade --force --alldeps \
 && pear install --onlyreqdeps PEAR_PackageFileManager2-beta \
 && ant dist -Ddoxygen.path=/usr/bin/doxygen -Dphp.path=/usr/bin/php
