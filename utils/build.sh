aef425ba0b9465f4f122f7f1f6d31ad225f052e4
Joachim Fritschi <jfritschi@freenet.de>
2021-02-28 21:55:57 +0000

    Download pear uncompressed during build

--- utils/build.sh
+++ utils/build.sh
@@ -4,6 +4,6 @@ yum -y update && yum -y install ant doxygen php-pear
 
 cd /app/utils \
 && pear channel-update pear \
-&& pear upgrade --force --alldeps \
+&& pear upgrade -Z --force --alldeps \
 && pear install --onlyreqdeps PEAR_PackageFileManager2-beta \
 && ant dist -Ddoxygen.path=/usr/bin/doxygen -Dphp.path=/usr/bin/php
